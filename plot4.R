feb146eb4993343b3a14c44f31a105274dae32f8
puttyomax <puttyomax@users.noreply.github.com>
2017-01-02 15:22:35 +0900

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,16 @@
+if(!exists("NEI")){
+        NEI <- readRDS("summarySCC_PM25.rds")
+}
+
+if(!exists("SCC")){
+        SCC <- readRDS("Source_Classification_Code.rds")
+}
+#downloading data
+library(ggplot2)
+neiscc<-merge(NEI,SCC,by="SCC")
+#takes some time
+coal<-subset(neiscc,grepl("Coal",EI.Sector))
+dcoal<-aggregate(Emissions ~ year, coal, sum)
+g<-ggplot(dcoal,aes(x=year,y=Emissions))
+g+geom_point()+geom_line(color="blue")
+#plotting
